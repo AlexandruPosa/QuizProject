038c4c4925b23b71d0a4e682997147a4ccfadb21
Posa Alexandru <alex.posa94@yahoo.com>
2025-04-10 23:18:59 +0300

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="footer">
+    <p>&copy; 2025 Quiz Project. All rights reserved.</p>
+  </footer>
+</template>
+  
+<style scoped lang="scss">
+  .footer {
+    text-align: center;
+    padding: 1rem 0;
+    background-color: var(--vt-c-black);
+    color: var(--vt-c-white);
+    border-top: 1px solid var(--color-border);
+    font-size: 0.875rem;
+  }              
+</style>                            
